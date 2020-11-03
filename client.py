fa0abcf3ba00291f2452713cc803396c3fc1814a
alexiuadrian <adi.alexiu@gmail.com>
2020-11-03 22:36:39 +0200

    Client - Server connection functionality

--- client.py
+++ client.py
@@ -6,5 +6,5 @@ ip_address = socket.gethostbyname(host_name)
 port = 666
 
 socket_object.connect(('192.168.0.105', port))  # Ip-ul server-ului
-print((socket_object.recv(1024)).decode('utf-8'))   # Decodeaza mesajul transmis din biti in string
+print((socket_object.recv(1024)).decode('utf-8'))   # Decodeaza mesajul transmis din biti in string folosind utf-8
 socket_object.close()
