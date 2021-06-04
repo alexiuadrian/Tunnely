e28b420ecfae6d4acbe6afa9e224b7c5f2bd11cb
Adrian Alexiu <adi.alexiu@gmail.com>
2021-06-04 19:58:50 +0300

    Added configuration for server

--- server.py
+++ server.py
@@ -4,6 +4,10 @@ import time
 import socket
 import subprocess
 import select
+from pytun import IFF_TAP
+
+info_to_sock = None
+info_to_tun = None
 
 # # Socket initialization
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,32 +18,38 @@ sock.bind(('192.168.0.106', 65432))
 sock.listen()
 conn, addr = sock.accept()
 
+# print(f'{addr} has connected!')
+
+# info_to_tun, addr = sock.recvfrom(56789)
+
 print(f'{addr[0]} has connected!')
 
-while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    print(f'Received {repr(data)}')
-    conn.sendall(data)
+#print(b"INFO TO TUN CONN " + info_to_tun)
 
-tun = pytun.TunTapDevice(name="TunnelyTUN0")
+# while True:
+#     data = conn.recv(1024)
+#     if not data:
+#         break
+#     print(f'Received {repr(data)}')
+#     conn.sendall(data)
+
+tun = pytun.TunTapDevice(flags=IFF_TAP)
 
 tun.addr = '10.8.0.1'
-#tun.dstaddr = '10.8.0.1'
 tun.netmask = '255.255.255.0'
 tun.mtu = 1500
 
 # Starting the TUN interface
 tun.up()
 
+print(f"The device {tun.name} is up!")
+
 # Making the TUN interface persistent so it can be active
 # after the user closed the program
 # tun.persist(True)
-print(f"The server's {tun.name} is up!")
+# print(f"The server's {tun.name} is up!")
+# tun.down()
 
-info_to_sock = None
-info_to_tun = None
 devices = [tun, sock]
 
 while True:
@@ -47,16 +57,23 @@ while True:
 
     if tun in read:
         info_to_sock = tun.read(tun.mtu)
+        # print(b"INFO TO SOCK 1 " + info_to_sock)
     
     if sock in read:
-        info_to_tun, addr = sock.listen()
+        # info_to_tun, addr = sock.listen()
+        info_to_tun, addr = sock.recvfrom(56789)
+        # sock.listen()
+        # info_to_tun, addr = sock.accept()
+        print(b"INFO TO TUN 1 " + info_to_tun)
     
-    if info_to_tun != None and tun in write:
+    if info_to_tun and tun in write:
         tun.write(info_to_tun)
+        print(b"INFO TO TUN 2 " + info_to_tun)
         info_to_tun = None
     
-    if info_to_sock != None and sock in write:
+    if info_to_sock and sock in write:
         sock.sendto(info_to_sock, ('192.168.0.103', 56789))
+        print(b"INFO TO SOCK 2 " + info_to_tun)
         info_to_sock = None
 
 
