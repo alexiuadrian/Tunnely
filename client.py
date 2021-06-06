7c681b8175299af2f4f63aaf21ddfde1b1875d60
Adrian Alexiu <adi.alexiu@gmail.com>
2021-06-06 19:17:00 +0300

    Funtional client

--- client.py
+++ client.py
@@ -6,20 +6,27 @@ from pytun import IFF_TAP
 import os
 import struct
 import fcntl
+#import pynetlinux
 
 '''
 Socket for packet transmission through the TUN interface
 '''
 
-# Socket initialization
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# tun = pynetlinux.tap.Tap()
+# tun.set_ip('10.8.0.2')
+# tun.set_netmask(24)
+# tun.up()
 
-sock.bind(('192.168.43.22', 56789))
+# Socket initialization
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock.bind(('192.168.0.106', 56789))
 
 #sock.connect(('192.168.0.106', 65432))
 
 # sock.sendto(b'Init message', ('192.168.0.106', 65432))
-sock.connect(('79.117.253.249', 65432))
+# sock.connect(('79.117.253.249', 65432))
+# msg, addr = sock.recvfrom(65432)
 # print(1)
 # sock.sendall(b'Init')
 # print(2)
@@ -29,24 +36,23 @@ sock.connect(('79.117.253.249', 65432))
 
 # subprocess.call(['sysctl', '-w', 'net.ipv4.ip_forward=1'])
 
-# tun = pytun.TunTapDevice(flags=pytun.IFF_TAP)
+tun = pytun.TunTapDevice(flags=pytun.IFF_TAP)
 
-# tun.addr = '10.8.0.2'
-# # tun.dstaddr = '10.8.0.1'
-# tun.netmask = '255.255.255.0'
-# tun.mtu = 1500
+tun.addr = '10.8.0.2'
+tun.netmask = '255.255.255.0'
+tun.mtu = 1500
 
 # # Starting the TUN interface
-# tun.up()
+tun.up()
 
 # print(f'The device {tun.name} is up!')
 
 # Making the TUN device the default route of the packets
-subprocess.call(['sudo', '/sbin/ip', 'route', 'add', 'default', 'gw', '10.8.0.1'])
+#os.system('sudo route add default gw 10.8.0.1')
 
-tun = os.open('/dev/net/tun', os.O_RDWR | os.O_NONBLOCK)
-ifr = struct.pack('16sH', 'tap0', 2 | 4096)
-fcntl.ioctl(tun, 0x400454ca, ifr)
+# tun = os.open('/dev/net/tun', os.O_RDWR | os.O_NONBLOCK)
+# ifr = struct.pack(b'16sH', b'tap0', 2 | 4096)
+# fcntl.ioctl(tun, 0x400454ca, ifr)
 
 '''
 Socket for packet transmission through the TUN interface
@@ -71,24 +77,29 @@ while True:
     read, write, x = select.select(devices, devices, [])
 
     if tun in read:
-        info_to_sock = tun.read(tun.mtu)
-        print(b"INFO TO SOCK 1" + info_to_sock)
+        # info_to_sock = tun.read(tun.mtu)
+        #info_to_sock = os.read(tun, 1500)
+        print(b"INFO TO SOCK 1", info_to_sock)
+        info_to_sock = tun.read(1500)
     
     if sock in read:
         # sock.listen()
         # info_to_tun, addr = sock.accept()
-        info_to_tun, addr = sock.recvfrom(65432)
-        print(b"INFO TO TUN 1" + info_to_tun)
+        #info_to_tun, addr = sock.recvfrom(65432)
+        print(b"INFO TO TUN 1", info_to_tun)
+        info_to_tun, ad = sock.recvfrom(65432)
     
     if info_to_tun and tun in write:
+        # tun.write(info_to_tun)
+        #os.write(tun, info_to_tun)
+        print(b"INFO TO TUN 2", info_to_tun)
         tun.write(info_to_tun)
-        print(b"INFO TO TUN 2" + info_to_tun)
         info_to_tun = None
     
     if info_to_sock and sock in write:
         sock.sendto(info_to_sock, ('79.117.253.249', 65432))
         #sock.sendall(info_to_sock)
-        print(b"INFO TO SOCK 2" + info_to_sock)
+        print(b"INFO TO SOCK 2", info_to_sock)
         info_to_sock = None
 
 # Stopping the TUN interface
