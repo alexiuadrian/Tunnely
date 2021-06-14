c5dacbaf3717ac07d2e063e1769379686a94a341
alexiuadrian <adi.alexiu@gmail.com>
2021-06-14 07:59:49 -0400

    Refactor code and added read from file

--- client.py
+++ client.py
@@ -7,6 +7,13 @@ import os
 import struct
 import fcntl    
 import sys
+from cryptography.fernet import Fernet
+
+
+def read_key_from_file(path_to_key):
+    f = open(path_to_key, "r")
+    
+    return str(f.read())
 
 # Command line arguments from the tunnely bash script
 path_to_key = sys.argv[1]
@@ -14,34 +21,23 @@ local_ip = sys.argv[2]
 local_port = int(sys.argv[3])
 remote_ip = sys.argv[4]
 remote_port = int(sys.argv[5])
+    
+
+key = read_key_from_file(path_to_key)
 
 '''
 Socket for packet transmission through the TUN interface
 '''
 
-# tun = pynetlinux.tap.Tap()
-# tun.set_ip('10.8.0.2')
-# tun.set_netmask(24)
-# tun.up()
-
 # Socket initialization
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+# Binding the socket to the local computer
 sock.bind((local_ip, local_port))
 
-#sock.connect(('192.168.0.106', 65432))
-
-# sock.sendto(b'Init message', ('192.168.0.106', 65432))
-# sock.connect(('79.117.253.249', 65432))
-# msg, addr = sock.recvfrom(65432)
-# print(1)
-# sock.sendall(b'Init')
-# print(2)
-# data = sock.recv(1024)
-# print(3)
-# print(f'Received {repr(data)}')
-
-# subprocess.call(['sysctl', '-w', 'net.ipv4.ip_forward=1'])
+'''
+Creating the TAP interface
+'''
 
 tap = pytun.TunTapDevice(flags=pytun.IFF_TAP)
 
@@ -49,32 +45,8 @@ tap.addr = '10.8.0.2'
 tap.netmask = '255.255.255.0'
 tap.mtu = 1500
 
-# # Starting the TUN interface
+# Starting the TAP interface
 tap.up()
-
-# print(f'The device {tun.name} is up!')
-
-# Making the TUN device the default route of the packets
-#os.system('sudo route add default gw 10.8.0.1')
-
-# tun = os.open('/dev/net/tun', os.O_RDWR | os.O_NONBLOCK)
-# ifr = struct.pack(b'16sH', b'tap0', 2 | 4096)
-# fcntl.ioctl(tun, 0x400454ca, ifr)
-
-'''
-Socket for packet transmission through the TUN interface
-'''
-
-# Socket initialization
-# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# Binding the socket to the TUN device
-# sock.bind(('10.8.0.2', 65535))
-
-
-# Making the TUN interface persistant so it can be active
-# after the user closed the program
-# tun.persist(True)
 
 info_to_sock = None
 info_to_tap = None
@@ -84,31 +56,29 @@ while True:
     read, write, x = select.select(devices, devices, [])
 
     if tap in read:
-        # info_to_sock = tun.read(tun.mtu)
-        #info_to_sock = os.read(tun, 1500)
         info_to_sock = tap.read(1500)
-        print(b"INFO TO SOCK 1", info_to_sock)
+        print("INFO TO SOCK 1 ", info_to_sock)
     
-    if sock in read:
-        # sock.listen()
-        # info_to_tun, addr = sock.accept()
-        #info_to_tun, addr = sock.recvfrom(65432)
-        info_to_tap, ad = sock.recvfrom(65432)
-        print(b"INFO TO TAP 1", info_to_tap)
+    if sock in read:        
+        info_to_tap, ad = sock.recvfrom(remote_port)
+        
+        f = Fernet(key)
+        info_to_tap = f.decrypt(info_to_tap)
+        
+        print("INFO TO TAP 1 ", info_to_tap)
     
     if info_to_tap and tap in write:
-        # tun.write(info_to_tun)
-        #os.write(tun, info_to_tun)
+        
         tap.write(info_to_tap)
+        print("INFO TO TAP 2 ", info_to_tap)
         info_to_tap = None
-        print(b"INFO TO TAP 2", info_to_tap)
     
     if info_to_sock and sock in write:
-        sock.sendto(info_to_sock, (remote_ip, remote_port))
-        #sock.sendall(info_to_sock)
-        print(b"INFO TO SOCK 2", info_to_sock)
+        print("INFO TO SOCK 2 ", info_to_sock)
+        
+        f = Fernet(key)
+        
+        encr_info_to_sock = f.encrypt(info_to_sock)
         info_to_sock = None
-
-# Stopping the TUN interface
-# tun.down()
-# print(f"The device {tun.name} is down!")
+        
+        sock.sendto(info_to_sock, (remote_ip, remote_port))
--- server.py
+++ server.py
@@ -9,8 +9,13 @@ import os
 import struct
 import fcntl
 import sys
-#sys.path.append('pathto/site-packages/pynetlinux')
-#import pynetlinux
+from cryptography.fernet import Fernet
+
+
+def read_key_from_file(path_to_key):
+    f = open(path_to_key, "r")
+    
+    return str(f.read())
 
 path_to_key = sys.argv[1]
 local_ip = sys.argv[2]
@@ -18,6 +23,7 @@ local_port = int(sys.argv[3])
 remote_ip = sys.argv[4]
 remote_port = int(sys.argv[5])
 
+key = read_key_from_file(path_to_key)
 info_to_sock = None
 info_to_tap = None
 
@@ -25,40 +31,19 @@ os.system('sudo sysctl -w net.ipv4.ip_forward=1')
 
 os.system('sudo iptables -t nat -A POSTROUTING -j MASQUERADE')
 
-#tun = pynetlinux.tap.Tap()
-#tun.set_ip('10.8.0.1')
-#tun.set_netmask(24)
-#tun.up()
+'''
+Socket for packet transmission through the TUN interface
+'''
 
-#subprocess.call(['iptables', '-t', 'nat', '-A', 'POSTROUTING', '-j', 'MASQUERADE'])
-
-# # Socket initialization
+# Socket initialization
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Binding the socket to the TUN device
+# Binding the socket to the local computer
 sock.bind((local_ip, local_port))
 
-# msg, addr = sock.recvfrom(56789)
-
-#sock.listen()
-#conn, addr = sock.accept()
-
-# print(f'{addr} has connected!')
-
-# info_to_tun, addr = sock.recvfrom(56789)
-
-# print(f'{addr[0]} has connected!')
-
-#print(b"INFO TO TUN CONN " + info_to_tun)
-
-# while True:
-#     data = conn.recv(1024)
-#     if not data:
-#         break
-#     print(f'Received {repr(data)}')
-#     conn.sendall(data)
-
-# subprocess.call(['sysctl', '-w', 'net.ipv4.ip_forward=1'])
+'''
+Creating the TAP interface
+'''
 
 tap = pytun.TunTapDevice(flags=IFF_TAP)
 
@@ -66,20 +51,8 @@ tap.addr = '10.8.0.1'
 tap.netmask = '255.255.255.0'
 tap.mtu = 1500
 
-# Starting the TUN interface
+# Starting the TAP interface
 tap.up()
-
-#print(f"The device {tun.name} is up!")
-
-# Making the TUN interface persistent so it can be active
-# after the user closed the program
-# tun.persist(True)
-# print(f"The server's {tun.name} is up!")
-# tun.down()
-
-#tun = os.open('/dev/net/tun', os.O_RDWR | os.O_NONBLOCK)
-#ifr = struct.pack(b'16sH', b'tap0', 2 | 4096)
-#fcntl.ioctl(tun, 0x400454ca, ifr)
 
 devices = [tap, sock]
 
@@ -87,53 +60,30 @@ while True:
     read, write, x = select.select(devices, devices, [])
 
     if tap in read:
-        # info_to_sock = tun.read(tun.mtu)
-        #info_to_sock = os.read(tun, 1500)
         info_to_sock = tap.read(1500)
         print("INFO TO SOCK 1 ", info_to_sock)
     
-    if sock in read:
-        # info_to_tun, addr = sock.listen()
-        #info_to_tun, addr = sock.recvfrom(56789)
-        # sock.listen()
-        # info_to_tun, addr = sock.accept()
+    if sock in read:        
         info_to_tap, ad = sock.recvfrom(remote_port)
+        
+        f = Fernet(key)
+        info_to_tap = f.decrypt(info_to_tap)
+        
         print("INFO TO TAP 1 ", info_to_tap)
     
     if info_to_tap and tap in write:
-        # tun.write(info_to_tun)
-        #os.write(tun, info_to_tun)
+        
         tap.write(info_to_tap)
         print("INFO TO TAP 2 ", info_to_tap)
         info_to_tap = None
     
     if info_to_sock and sock in write:
-        #sock.sendto(info_to_sock, (addr[0], 56789))
-        print("INFO TO SOCK 2 ", info_to_tap)
-        sock.sendto(info_to_sock, (remote_ip, remote_port))
+        print("INFO TO SOCK 2 ", info_to_sock)
+        
+        f = Fernet(key)
+        
+        encr_info_to_sock = f.encrypt(info_to_sock)
         info_to_sock = None
-
-
-
-'''
-THIS SHOULD BE RUN ONLY ON THE CLIENT
-'''
-# Making the TUN device the default route of the packets
-# subprocess.call(['/sbin/ip', 'route', 'add', 'default', 'dev', 'TunnelyTUN0'])
-
-# Reading from the TUN device
-# buf = tun.read(tun.mtu)
-
-
-
-# while True:
-#     buf = tun.read(tun.mtu)
-#     # tun.write(buf)
-#     print(f'Continut buffer: {buf}')
-
-# Writing to the TUN device
-# tun.write(buf)
-
-# Stopping the TUN interface
-# tun.down()
-# print(f"The device {tun.name} is down!")
+        
+        sock.sendto(info_to_sock, (remote_ip, remote_port))
+        
